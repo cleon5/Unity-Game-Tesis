201524419e87a7ce3431983a71f436b0458eff8c
cleon5 <cleon5@ucol.mx>
2022-09-02 13:08:56 -0500

    Update Enemys

--- Assets/Scrips/Enemigo.cs
+++ Assets/Scrips/Enemigo.cs
@@ -10,7 +10,9 @@ public class Enemigo : MonoBehaviour
 
     float velocidad = 5f;
     bool atacando = false;
-    float rango_ataque = 5f;
+    float rango_ataque = 4f;
+    float distancia = 15f;
+    float diferencia;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -21,23 +23,30 @@ public class Enemigo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Mathf.Abs(transform.position.x - target.transform.position.x) > rango_ataque && !atacando)
+        diferencia = Mathf.Abs(transform.position.x - target.transform.position.x);
+        if (diferencia > distancia)
         {
+            print("no se deberia mover");
+        }
+
+        else if (diferencia > rango_ataque && !atacando)
+        {
+            animator.SetBool("Movimiento", true);
             if (transform.position.x < target.transform.position.x)
             {
                 transform.Translate(Vector3.right * velocidad * Time.deltaTime);
                 transform.rotation = Quaternion.Euler(0, 0, 0);
-                animator.SetTrigger("Movimiento");
             }
             else
             {
-                animator.SetTrigger("Movimiento");
+
                 transform.Translate(Vector3.right * velocidad * Time.deltaTime);
                 transform.rotation = Quaternion.Euler(0, 180, 0);
             }
         }
         else
         {
+            animator.SetBool("Movimiento", false);
             if (!atacando)
             {
                 animator.SetTrigger("Ataque");
